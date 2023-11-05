b3471f1765594902660bc392142be05da99f5754
Mattias Jansson <mjansson@gmail.com>
2023-11-05 17:47:18 +0100

    Update build scripts

--- configure.py
+++ configure.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.join('build', 'ninja'))
 
 import generator
 
-dependlibs = ['vector', 'foundation']
+dependlibs = ['mesh', 'vector', 'foundation']
 
 generator = generator.Generator(project = 'mesh', dependlibs = dependlibs, variables = [('bundleidentifier', 'com.maniccoder.mesh.$(binname)')])
 target = generator.target
@@ -27,13 +27,10 @@ mesh_lib = generator.lib(module = 'mesh', sources = mesh_sources + extrasources)
 if not target.is_ios() and not target.is_android() and not target.is_tizen():
   configs = [config for config in toolchain.configs if config not in ['profile', 'deploy']]
 
-#No test cases if we're a submodule
-if generator.is_subninja():
+if generator.skip_tests():
   sys.exit()
 
 includepaths = generator.test_includepaths()
-
-linklibs = ['test'] + dependlibs
 
 test_cases = []
 if toolchain.is_monolithic() or target.is_ios() or target.is_android() or target.is_tizen():
@@ -57,16 +54,19 @@ if toolchain.is_monolithic() or target.is_ios() or target.is_android() or target
     test_resources = [os.path.join('all', 'tizen', item) for item in [
       'tizen-manifest.xml', os.path.join('res', 'tizenapp.png')
     ]]
+  dependlibs = ['test'] + dependlibs
   if target.is_macos() or target.is_ios() or target.is_android() or target.is_tizen():
-    generator.app(module = '', sources = [os.path.join(module, 'main.c') for module in test_cases] + test_extrasources, binname = 'test-all', basepath = 'test', implicit_deps = [mesh_lib], libs = linklibs, resources = test_resources, includepaths = includepaths)
+    generator.app(module = '', sources = [os.path.join(module, 'main.c') for module in test_cases] + test_extrasources, binname = 'test-mesh', basepath = 'test', implicit_deps = [mesh_lib], libs = dependlibs, dependlibs = dependlibs, resources = test_resources, includepaths = includepaths)
   else:
-    generator.bin(module = '', sources = [os.path.join(module, 'main.c') for module in test_cases] + test_extrasources, binname = 'test-all', basepath = 'test', implicit_deps = [mesh_lib], libs = linklibs, resources = test_resources, includepaths = includepaths)
+    generator.bin(module = '', sources = [os.path.join(module, 'main.c') for module in test_cases] + test_extrasources, binname = 'test-mesh', basepath = 'test', implicit_deps = [mesh_lib], libs = dependlibs, dependlibs = dependlibs, resources = test_resources, includepaths = includepaths)
 else:
   #Build one binary per test case
-  generator.bin(module = 'all', sources = ['main.c'], binname = 'test-all', basepath = 'test', implicit_deps = [mesh_lib], libs = ['mesh'] + dependlibs, includepaths = includepaths)
+  if not generator.is_subninja:
+    generator.bin(module = 'all', sources = ['main.c'], binname = 'test-all', basepath = 'test', implicit_deps = [mesh_lib], libs = dependlibs, dependlibs = dependlibs, includepaths = includepaths)
+  dependlibs = ['test'] + dependlibs
   for test in test_cases:
     if target.is_macos():
       test_resources = [os.path.join('osx', item) for item in ['test-' + test + '.plist', 'Images.xcassets', 'test-' + test + '.xib']]
-      generator.app(module = test, sources = ['main.c' ], binname = 'test-' + test, basepath = 'test', implicit_deps = [mesh_lib], libs = linklibs, resources = test_resources, includepaths = includepaths)
+      generator.app(module = test, sources = ['main.c' ], binname = 'test-' + test, basepath = 'test', implicit_deps = [mesh_lib], libs = dependlibs, dependlibs = dependlibs, resources = test_resources, includepaths = includepaths)
     else:
-      generator.bin(module = test, sources = ['main.c' ], binname = 'test-' + test, basepath = 'test', implicit_deps = [mesh_lib], libs = linklibs, includepaths = includepaths)
+      generator.bin(module = test, sources = ['main.c' ], binname = 'test-' + test, basepath = 'test', implicit_deps = [mesh_lib], libs = dependlibs, dependlibs = dependlibs, includepaths = includepaths)
